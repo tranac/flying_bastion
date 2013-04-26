49c320527d51bc198eeb497b3ee2db665b233f70
Angelica Huyen Tran <tranac@usc.edu>
2013-04-26 04:31:03 -0700

    Added gargoyle images

--- gargoyle.cpp
+++ gargoyle.cpp
@@ -11,20 +11,16 @@ Gargoyle::~Gargoyle()
 
 void Gargoyle::move()
 {
-	//every other move, change image
-	if(x_ % 2)
-		setPixmap(*pic2_);
-	else
-		setPixmap(*pic_);
-		
 	//if inline with player & not a third of the way across screen, match player y-coordinate
 	if((y_ > 200))
 	{
 		y_ = p_->getY();
 	}
 	else
+	{
 		y_ = y_ + vy_;
-
+		setPixmap(*pic2_);
+	}
 	//change xy coordinates
 	x_ = x_ + vx_;
 	setPos(x_,y_);
